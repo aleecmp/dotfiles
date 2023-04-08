cca6260ccc82b4a89848e1558b97f41225b855b1
aleecmp <ale.cmpb@gmail.com>
2023-04-07 21:30:10 -0400

    first commit

---
+++ nvim/nvim/after/plugin/markdown-preview.lua
@@ -0,0 +1,8 @@
+vim.g.mkdp_auto_close = 1
+vim.g.mkdp_refresh_slow = 0
+vim.g.mkdp_command_for_global = 1
+vim.g.mkdp_open_to_the_world = 1
+vim.g.mkdp_echo_preview_url = 1
+vim.g.mkdp_filetypes = 'markdown'
+vim.g.mkdp_page_title = '${name}'
+vim.g.mkdp_theme = 'dark'
